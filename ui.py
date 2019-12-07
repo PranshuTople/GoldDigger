685340c3dbf6300919f48ca3b22057c1dec7d086
PranshuTople <pranshutople@gmail.com>
2019-12-07 14:12:33 +0530

    added screenshot script

--- ui.py
+++ ui.py
@@ -1,5 +1,7 @@
 import tkinter
+from selenium import webdriver
 
+"""
 window = tkinter.Tk()
 
 window.title("GoldDigger")
@@ -7,3 +9,14 @@ window.title("GoldDigger")
 label = tkinter.Label (window, text = "Hello World").pack()
 
 window.mainloop()
+"""
+
+#options = webdriver.Firefox()
+#options.binary_location = "/usr/bin/geckodriver"
+driver = webdriver.Firefox()
+
+driver.get('https://paytm.com/digitalgold')
+driver.save_screenshot("screenshot.png")
+
+#driver.close()
+
